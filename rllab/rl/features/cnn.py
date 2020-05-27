0e526759f440628b68246d6a204995dc73816976
Jason Gu <jason@Jasons-MacBook-Pro.local>
2020-05-27 23:19:31 +0800

    add ppo

--- rllab/rl/features/cnn.py
+++ rllab/rl/features/cnn.py
@@ -51,3 +51,64 @@ class conv_only(nn.Sequential):
 
 
 
+class nature_cnn(nn.Sequential):
+    def __init__(self, input_shape, input_format='NCHW'):
+        """
+        CNN from Nature paper.
+        """
+
+        def Conv2d(in_channels, out_channels, kernel_size, stride, init_scale=1.0):
+            conv2d = nn.Conv2d(in_channels, out_channels, (kernel_size, kernel_size), stride=stride)
+            nn.init.orthogonal_(conv2d.weight.data, gain=init_scale)
+            nn.init.constant_(conv2d.bias.data, 0.0)
+            return conv2d
+
+        assert input_format == 'NCHW' or 'NHWC'
+        l = []
+        if input_format == 'NHWC':
+            l.append(nn.Permute((0, 3, 1, 2)))
+            input_shape = (input_shape[0], input_shape[3], input_shape[1], input_shape[2])
+
+        l.append(nn.Float()),
+        l.append(nn.TrueDivide(255.))
+
+        # conv layers
+        in_channels = input_shape[1]
+        output_size = input_shape[2:4]
+
+        conv = Conv2d(in_channels, 32, 8, 4, init_scale=np.sqrt(2))
+        output_size = nn.eval_conv_output_size(conv, output_size)
+        l.append(conv)
+        l.append(nn.ReLU)
+
+        conv = Conv2d(32, 64, 4, 2, init_scale=np.sqrt(2))
+        output_size = nn.eval_conv_output_size(conv, output_size)
+        l.append(conv)
+        l.append(nn.ReLU)
+
+        conv = Conv2d(64, 64, 3, 1, init_scale=np.sqrt(2))
+        output_size = nn.eval_conv_output_size(conv, output_size)
+        l.append(conv)
+        l.append(nn.ReLU)
+
+        conv_output_shape = (input_shape[0], 64) + output_size
+
+        # fc
+        l.append(nn.Flatten())
+        flatten_features = int(np.prod(conv_output_shape[1:]))
+        x = nn.Linear(flatten_features, 512)
+        nn.init.orthogonal_(x.weight.data, gain=np.sqrt(2))
+        nn.init.constant_(x.bias.data, 0.0)
+        l.append(x)
+        l.append(nn.ReLU)
+
+        layers = [('{}_{}'.format(l[i].__class__.__name__.lower(), i), l[i]) for i in range(len(l))]
+        super(nature_cnn, self).__init__(OrderedDict(layers))
+
+        self.output_shape = (input_shape[0], flatten_features)
+
+
+
+@register('cnn')
+class cnn(nature_cnn):
+    pass
